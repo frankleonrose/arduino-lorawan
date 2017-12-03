06d6a894e4343d9c3cacfec82e3640213fb72a40
Frank Leon Rose <frankleonrose@gmail.com>
2017-12-03 11:24:47 -0500

    Removed files added to satisfy dependency

--- src/mcciadk_baselib.h
+++
@@ -1,2 +0,0 @@
-// Stand in version of this file until the real one is committed
-#define McciAdkLib_MultiSzIndex(list, index) "<<unimplemented>>"
--- src/mcciadk_env.h
+++
@@ -1,4 +0,0 @@
-// Stand in version of this file until the real one is committed
-#define MCCIADK_BEGIN_DECLS
-#define MCCIADK_END_DECLS
-#define MCCIADK_LENOF(x) (sizeof(x) / sizeof(x[0]))
